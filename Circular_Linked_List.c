d18de27b3f686600727e1463f43a94643cc4e9b9
Simanta Roy <roy35-909@diu.edu.bd>
2022-12-14 19:56:50 +0600

    Updated 14.12.2022

--- Circular_Linked_List.c
+++ Circular_Linked_List.c
@@ -2,6 +2,12 @@
     Simanta Kumar Roy
     221-35-909
     roy35-909@diu.edu.bd
+
+    Ques:Create a circular linked list with a Sentinel, ,Delete Second element
+         from this linked list and print all elements.To create and print the
+         Linked list, using a loop is mandatory.
+
+
  */
 #include <stdio.h>
 #include <malloc.h>
@@ -12,6 +18,7 @@ void main()
   struct node
     {
         int num;
+        int pos;
         int *ptr;
    };
     typedef struct node NODE;
@@ -22,23 +29,25 @@ void main()
 
     head  = (NODE *)malloc(sizeof(NODE));
     head->num = -1;
+    head->pos = count;
     first = temp = head;
 
     printf("\n\n######### Enter Data For Your Circular Linked List As Much As you want.#########\n");
     printf("\n********* If you Want to exit give any Char Data as Input *********\n");
     while (choice)
     {
-
+            count++;
             head  = (NODE *)malloc(sizeof(NODE));
             printf("\nData For Index [%d] =  ",count);
 
             if(!scanf("%d", &head-> num))
                 break;
+            head->pos = count;
             temp->ptr = head;
             temp = head;
 
-        fflush(stdin);
-        count++;
+            fflush(stdin);
+
     }
 
 
@@ -51,17 +60,47 @@ void main()
 
     printf("\n\n\n\n\n \t\t########## Printing Circular Linked List ##########\n\n\n\n\n\n");
     //Reset Value For printing Form First Node
-    count =0;
+
     temp = first;
 
     while (1)
     {
-        printf("\t\t[%d]   Address: %d     Data: %d     Next: %d \n",count,temp,temp->num, temp->ptr);
-        count++;
+        printf("\t\t[%d]   Address: %d     Data: %d     Next: %d \n",temp->pos,temp,temp->num, temp->ptr);
+
         temp = temp->ptr;
         if(temp->num == -1)
             break;
     }
 
-    printf("\n\n \t\tTotal Number Of Nodes = %d\n", count);
+
+    printf("\n\n\n \t\t########## Deleted 2nd Element From Circular Linked List ##########\n\n");
+
+    temp = first;
+    NODE *temp0;
+    while(1)
+    {
+
+        if(temp->pos == 2)
+        {
+            temp0->ptr = temp->ptr;
+            break;
+        }
+
+        temp0 = temp;
+        temp = temp->ptr;
+
+    }
+    printf("\n\n\n \t\t########## Printing Circular Linked List ##########\n\n\n\n\n\n");
+
+    temp = first;
+
+    while (1)
+    {
+        printf("\t\t[%d]   Address: %d     Data: %d     Next: %d \n",temp->pos,temp,temp->num, temp->ptr);
+
+        temp = temp->ptr;
+        if(temp->num == -1)
+            break;
+    }
+
 }
